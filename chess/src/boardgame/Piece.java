9193be3a9292a365dab0b2c16017f94d5ca3e887
Kauan <cvitorcordeiro@gmail.com>
2024-06-29 11:44:18 -0300

    possible movies of a piece

--- chess/src/boardgame/Piece.java
+++ chess/src/boardgame/Piece.java
@@ -1,6 +1,8 @@
 package boardgame;
 
-public class Piece {
+import java.io.PipedOutputStream;
+
+public abstract class Piece {
 	
 	protected Position position;
 	private Board board;
@@ -17,11 +19,22 @@ public class Piece {
 	}
 
 
+	public abstract boolean[][] possibleMoves();
 
-	
-	
-	
-	
-	
+	public boolean possibleMove(Position position){
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
 
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if(mat[i][j]){
+						return true;
+				}
+			}
+		}
+		return false;
+
+    }
 }
--- chess/src/chess/ChessPiece.java
+++ chess/src/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 
-public class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece{
 	
 	private Color color;
 
@@ -17,9 +17,9 @@ public class ChessPiece extends Piece{
 		return color;
 	}
 
-	
-	
-	
-	
+
+
+
+
 
 }
