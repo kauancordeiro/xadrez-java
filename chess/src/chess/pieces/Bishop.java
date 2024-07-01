cd97f8c00ee323bda280e69d450216e0ffdd735b
Kauan <cvitorcordeiro@gmail.com>
2024-06-30 22:01:10 -0300

    bishop

--- chess/src/chess/pieces/Bishop.java
+++ chess/src/chess/pieces/Bishop.java
@@ -1,2 +1,67 @@
-package chess.pieces;public class Bishop {
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece {
+
+    public Bishop(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0,0);
+
+        // nw (cima diagona direita)
+        p.setValues(position.getRow() - 1, position.getColumn() - 1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsApiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() - 1, p.getColumn() - 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // ne (cima diagonal esquerda)
+        p.setValues(position.getRow() - 1, position.getColumn() + 1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsApiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() -1, p.getColumn() + 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // se (baixo diagonal direita)
+        p.setValues(position.getRow() + 1, position.getColumn() + 1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsApiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() + 1, p.getColumn() + 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // sw (baixo diagonal equerda)
+        p.setValues(position.getRow() + 1, position.getColumn() - 1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsApiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() + 1, p.getColumn() - 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        return mat;
+    }
+
+    public String toString(){
+        return "B";
+    }
 }
