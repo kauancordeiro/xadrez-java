f93a1a72d0261a95c1aea21e58cdde2036809e46
Kauan <cvitorcordeiro@gmail.com>
2024-07-01 22:59:18 -0300

    rook

--- chess/src/chess/pieces/King.java
+++ chess/src/chess/pieces/King.java
@@ -2,12 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
-    public King(Board board, Color color) {
+
+    private ChessMatch chessMatch;
+
+    public King(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     public String toString(){
@@ -17,6 +22,11 @@ public class King extends ChessPiece {
     public boolean canMove(Position position){
         ChessPiece p = (ChessPiece)getBoard().piece(position);
         return p == null || p.getColor() != getColor();
+    }
+
+    private boolean testRookCastling(Position position){
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
     }
 
     @Override
@@ -75,6 +85,39 @@ public class King extends ChessPiece {
         p.setValues(position.getRow() + 1, position.getColumn() +1);
         if(getBoard().positionExists(p) && canMove(p)){
             mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // special move castling (rook)
+        if(getMoveCount() == 0 && !chessMatch.getCheck()){
+            // rook kingside / pequeno (ao lado do rei)
+            Position posT1 = new Position(position.getRow(), position.getColumn() + 3);
+            if(testRookCastling(posT1)){
+                // veificar se as duas casas ao lado estão vazia para o rook
+                Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+
+                if(getBoard().piece(p1) == null && getBoard().piece(p2) == null)
+                {
+                    mat[position.getRow()][position.getColumn() + 2] = true;
+                }
+
+            }
+
+            // rook queenside / longo (ao lado da rainha)
+            Position posT2 = new Position(position.getRow(), position.getColumn() - 4);
+            if(testRookCastling(posT1)){
+                // veificar se as tres casas ao lado estão vazia para o rook longo
+                Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+                Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+
+                if(getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null)
+                {
+                    mat[position.getRow()][position.getColumn() - 2] = true;
+                }
+
+            }
+
         }
 
         return mat;
